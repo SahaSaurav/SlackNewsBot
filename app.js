40556e43e2e376384f7dc4ded82e50ceeb863301
Ahmed Abdelmoneim <ahmedh.abdelmoneim@gmail.com>
2018-12-01 14:45:46 -0800

    inital app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express  		    = require('express');
+const app      		    = express();
+const port    	        = process.env.PORT || 8080;
+const morgan            = require('morgan');
+const bodyParser       	= require('body-parser');
+
+// required for body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Run server
+app.listen(process.env.PORT || port, function() {
+    console.log('The magic happens on port ' + port);
+});
