d6338c1f05422f30b19833adec21789ad0a3524b
seifghazi <seifghazi@gmail.com>
2018-12-01 18:05:31 -0800

    clean up message controller

--- controller/message_controller.js
+++ controller/message_controller.js
@@ -1,41 +1,5 @@
-const fetchNews         = require('../services/request_service');
 require('dotenv').config()
-
-
-
-
-// // Send Message
-// web.chat.postMessage({ channel: channelID, text: 'Hello there' })
-//   .then((res) => {
-//     // `res` contains information about the posted message
-//     console.log('Message sent: ', res.ts);
-//   })
-//   .catch(console.error);
-//
-// // Send Message with attachment
-// web.chat.postMessage({
-//   channel: channelID,
-//   text: 'BREAKING NEWS :partyparrot',
-//   attachments: [
-//     {
-//       "fallback": "News Story",
-//       "color": "#36a64f",
-//       "author_name": "Bobby Tables",
-//       "title": "News Story",
-//       "title_link": "https://api.slack.com/",
-//       "text": "Optional text that appears within the attachment",
-//       "footer": "Slack API",
-//       "ts": 123456789
-//     }
-//   ]
-// })
-//   .then((res) => {
-//     // `res` contains information about the posted message
-//     console.log('Message sent: ', res.ts);
-//   })
-//   .catch(console.error);
-
-
+const fetchNews         = require('../services/request_service');
 
 function objectMapper(body) {
 	const articles = body.articles;
