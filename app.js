4e41a045060996cceb46f2ca818e39f1316e297a
seifghazi <seifghazi@gmail.com>
2018-12-01 16:14:33 -0800

    first message

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port    	        = process.env.PORT || 8080;
 const morgan            = require('morgan');
 const bodyParser       	= require('body-parser');
 const routes            = require('./routes/request_routes');
+const { WebClient }     = require('@slack/client');
 require('dotenv').config()
-
 
 // Routes
 app.use(routes);
@@ -13,18 +13,15 @@ app.use(routes);
 // required for body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
-const { WebClient } = require('@slack/client');
-
-// An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
+// Slack access token, Channel ID
 const token = process.env.SLACK_TOKEN;
-
 const web = new WebClient(token);
 
-// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = process.env.CHANNEL_ID;
+
+const channelID = process.env.CHANNEL_ID;
 
 // See: https://api.slack.com/methods/chat.postMessage
-web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
+web.chat.postMessage({ channel: channelID, text: 'Hello there' })
   .then((res) => {
     // `res` contains information about the posted message
     console.log('Message sent: ', res.ts);
