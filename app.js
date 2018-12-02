1e609ce0216ee8c1080b8530d43babff1446a581
seifghazi <seifghazi@gmail.com>
2018-12-01 16:55:16 -0800

    refactor messages controller

--- app.js
+++ app.js
@@ -3,50 +3,11 @@ const app      		      = express();
 const port    	        = process.env.PORT || 8080;
 const morgan            = require('morgan');
 const bodyParser       	= require('body-parser');
-const fetchNews         = require('./services/request_service');
 const { WebClient }     = require('@slack/client');
 require('dotenv').config()
 
 // required for body-parser
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Slack configuration
-const token = process.env.SLACK_TOKEN;
-const web = new WebClient(token);
-const channelID = process.env.CHANNEL_ID;
-
-// // Send Message
-// web.chat.postMessage({ channel: channelID, text: 'Hello there' })
-//   .then((res) => {
-//     // `res` contains information about the posted message
-//     console.log('Message sent: ', res.ts);
-//   })
-//   .catch(console.error);
-//
-// // Send Message with attachment
-// web.chat.postMessage({
-//   channel: channelID,
-//   text: 'BREAKING NEWS :partyparrot',
-//   attachments: [
-//     {
-//       "fallback": "News Story",
-//       "color": "#36a64f",
-//       "author_name": "Bobby Tables",
-//       "title": "News Story",
-//       "title_link": "https://api.slack.com/",
-//       "text": "Optional text that appears within the attachment",
-//       "footer": "Slack API",
-//       "ts": 123456789
-//     }
-//   ]
-// })
-//   .then((res) => {
-//     // `res` contains information about the posted message
-//     console.log('Message sent: ', res.ts);
-//   })
-//   .catch(console.error);
-
-fetchNews(fetchNews);
 
 // Run server
 app.listen(process.env.PORT || port, function() {
--- controller/message_controller.js
+++ controller/message_controller.js
@@ -1,4 +1,43 @@
 
+const fetchNews         = require('./services/request_service');
+require('dotenv').config()
+
+// Slack configuration
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+const channelID = process.env.CHANNEL_ID;
+
+// Send Message
+web.chat.postMessage({ channel: channelID, text: 'Hello there' })
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
+
+// Send Message with attachment
+web.chat.postMessage({
+  channel: channelID,
+  text: 'BREAKING NEWS :partyparrot',
+  attachments: [
+    {
+      "fallback": "News Story",
+      "color": "#36a64f",
+      "author_name": "Bobby Tables",
+      "title": "News Story",
+      "title_link": "https://api.slack.com/",
+      "text": "Optional text that appears within the attachment",
+      "footer": "Slack API",
+      "ts": 123456789
+    }
+  ]
+})
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
+
 
 function objectMapper(body) {
 
