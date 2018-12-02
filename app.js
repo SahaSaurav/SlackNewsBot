1c3c2cfb30716028dcf7c8fcc25f789cd60c255c
Ahmed Abdelmoneim <ahmedh.abdelmoneim@gmail.com>
2018-12-01 17:47:40 -0800

    add get route

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-const express  		    = require('express');
-const app      		    = express();
-const port    	        = process.env.PORT || 8080;
-const morgan            = require('morgan');
-const bodyParser       	= require('body-parser');
-const CronJob           = require('cron').CronJob;
 require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const CronJob = require('cron').CronJob;
 
 // required for body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Cron Job config
-console.log('Before job instantiation');
-const job = new CronJob('0 9 * * *', function() {
-	// Main workflow
-});
-console.log('After job instantiation');
-job.start();
-
-// respond with "hello world" when a GET request is made to the homepage
+// GET page for heroku hosting, schedule rone job on open
 app.get('/', function (req, res) {
-    res.send('hello world')
-  })
+    // Cron Job config
+    const job = new CronJob('0 9 * * *', function () {
+        // Main workflow
+    });
+    job.start();
+
+    res.send('hello world');
+})
 
 // Run server
-app.listen(process.env.PORT || port, function() {
+app.listen(process.env.PORT || port, function () {
     console.log('The magic happens on port ' + port);
 });
